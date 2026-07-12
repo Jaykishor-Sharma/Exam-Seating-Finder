7480690940de420d602fa87dd77d0ee6c7c642a4
Jaykishor-Sharma <jaykishorsharma325@gmail.com>
2026-07-12 21:05:21 +0530

    Add files via upload

---
+++ User.java
@@ -0,0 +1,32 @@
+package myFirstBackendProject;
+
+public class User {
+	private String Enrollment;
+	private String Building;
+	private String Room;
+	
+	public String getEnrollment() {
+		return Enrollment;
+	}
+	
+	public void setEnrollment(String Enrollment) {
+		this.Enrollment = Enrollment;
+	}
+	
+	public String getBuilding() {
+		return Building;
+	}
+	
+	public void setBuilding(String Building) {
+		this.Building = Building;
+	}
+	
+	public String getRoom() {
+		return Room;
+	}
+	
+	public void setRoom(String Room) {
+		this.Room = Room;
+	}
+
+}
